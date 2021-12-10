4806d3c9c42af7686a57a37c335b5c18c3d1763d
Faik Turan <turanfaik@hotmail.com>
2021-12-10 16:25:48 +0300

    Add project files.

---
+++ MCD_CheckBoxListInceleme/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace MCD_CheckBoxListInceleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtUrunAciklama = new System.Windows.Forms.TextBox();
+            this.txtYazar = new System.Windows.Forms.TextBox();
+            this.txtUrunKategori = new System.Windows.Forms.TextBox();
+            this.txtStokAdet = new System.Windows.Forms.TextBox();
+            this.txtUrunAdi = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pctUrunResim = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkListBox = new System.Windows.Forms.CheckedListBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctUrunResim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtUrunAciklama);
+            this.groupBox1.Controls.Add(this.txtYazar);
+            this.groupBox1.Controls.Add(this.txtUrunKategori);
+            this.groupBox1.Controls.Add(this.txtStokAdet);
+            this.groupBox1.Controls.Add(this.txtUrunAdi);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.pctUrunResim);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(44, 171);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(768, 346);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Seçilen ÜrünDetay Bilgisi";
+            // 
+            // txtUrunAciklama
+            // 
+            this.txtUrunAciklama.Location = new System.Drawing.Point(414, 157);
+            this.txtUrunAciklama.Multiline = true;
+            this.txtUrunAciklama.Name = "txtUrunAciklama";
+            this.txtUrunAciklama.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtUrunAciklama.Size = new System.Drawing.Size(348, 170);
+            this.txtUrunAciklama.TabIndex = 4;
+            // 
+            // txtYazar
+            // 
+            this.txtYazar.Location = new System.Drawing.Point(414, 125);
+            this.txtYazar.Name = "txtYazar";
+            this.txtYazar.Size = new System.Drawing.Size(203, 26);
+            this.txtYazar.TabIndex = 3;
+            // 
+            // txtUrunKategori
+            // 
+            this.txtUrunKategori.Location = new System.Drawing.Point(414, 93);
+            this.txtUrunKategori.Name = "txtUrunKategori";
+            this.txtUrunKategori.Size = new System.Drawing.Size(203, 26);
+            this.txtUrunKategori.TabIndex = 3;
+            // 
+            // txtStokAdet
+            // 
+            this.txtStokAdet.Location = new System.Drawing.Point(414, 61);
+            this.txtStokAdet.Name = "txtStokAdet";
+            this.txtStokAdet.Size = new System.Drawing.Size(203, 26);
+            this.txtStokAdet.TabIndex = 3;
+            // 
+            // txtUrunAdi
+            // 
+            this.txtUrunAdi.Location = new System.Drawing.Point(414, 29);
+            this.txtUrunAdi.Name = "txtUrunAdi";
+            this.txtUrunAdi.Size = new System.Drawing.Size(203, 26);
+            this.txtUrunAdi.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(269, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 18);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Açıklama:";
+            // 
+            // pctUrunResim
+            // 
+            this.pctUrunResim.Location = new System.Drawing.Point(17, 32);
+            this.pctUrunResim.Name = "pctUrunResim";
+            this.pctUrunResim.Size = new System.Drawing.Size(240, 295);
+            this.pctUrunResim.TabIndex = 1;
+            this.pctUrunResim.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(269, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 18);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Yazar:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(269, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 18);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Ürün Kategori:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(269, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 18);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Stok Adet:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(269, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ürün Adı:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(228, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(311, 58);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ürün Listesi";
+            // 
+            // chkListBox
+            // 
+            this.chkListBox.FormattingEnabled = true;
+            this.chkListBox.Location = new System.Drawing.Point(316, 70);
+            this.chkListBox.Name = "chkListBox";
+            this.chkListBox.Size = new System.Drawing.Size(164, 88);
+            this.chkListBox.TabIndex = 8;
+            this.chkListBox.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.chkListBox_ItemCheck);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 623);
+            this.Controls.Add(this.chkListBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Checked List Box";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctUrunResim)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtUrunAciklama;
+        private System.Windows.Forms.TextBox txtYazar;
+        private System.Windows.Forms.TextBox txtUrunKategori;
+        private System.Windows.Forms.TextBox txtStokAdet;
+        private System.Windows.Forms.TextBox txtUrunAdi;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pctUrunResim;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox chkListBox;
+    }
+}
+
---
+++ MCD_CheckBoxListInceleme/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MCD_CheckBoxListInceleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            checkedBoxListDoldurV1();
+        }
+
+        private void checkedBoxListDoldurV1()
+        {
+            foreach (var item in database.UrunTablo)
+            {
+                chkListBox.Items.Add(item);
+            }
+        }
+
+        private void chkListBox_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            CheckedListBox checkedListBox = (CheckedListBox)sender;
+            if (e.NewValue == CheckState.Checked)
+            {
+                urun secilenUrun = checkedListBox.Items[e.Index] as urun;
+                pctUrunResim.Image = Image.FromFile(secilenUrun.urunResim);
+                txtUrunAdi.Text = secilenUrun.urunAdi;
+                txtUrunKategori.Text = secilenUrun.urunKategori;
+                txtStokAdet.Text = secilenUrun.stokAdet.ToString();
+                txtUrunAciklama.Text = secilenUrun.aciklama;
+                txtYazar.Text = secilenUrun.yazar;
+            }
+
+            for (int i = 0; i < chkListBox.Items.Count; i++)
+            {
+                if (i == e.Index)
+                {
+                    //Üzerinde bulunan kayıt
+                }
+                else
+                {
+                    chkListBox.SetItemChecked(i, false);
+                }
+            }
+
+
+        }
+    }
+}
